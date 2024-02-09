046756de3a677a16a961fde83d303dddd64b757c
Git And Github <yashlende93@gmail.com>
2024-02-09 16:13:29 +0530

    Initial commit

---
+++ src/Process2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Process2 {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("");
+    }
+
+}
